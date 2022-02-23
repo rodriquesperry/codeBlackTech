ad965f5c0f66856d74c864b3277c1f2955b48222
Rodriques <rodriques.perry@gmail.com>
2022-02-23 16:59:07 -0600

    update pom file with thymeleaf, add controller and template for Hello

---
+++ src/main/java/tech/codeblack/codeblacktech/CodeBlackTechApplication.java
@@ -0,0 +1,13 @@
+package tech.codeblack.codeblacktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBlackTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBlackTechApplication.class, args);
+    }
+
+}
