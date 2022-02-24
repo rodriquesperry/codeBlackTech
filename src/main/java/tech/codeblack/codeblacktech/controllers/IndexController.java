4041ca5c50d4d8c714dd374e6d4aa24dcecc4b97
Rodriques <rodriques.perry@gmail.com>
2022-02-24 05:52:12 -0600

    update: partials.html, css stylesheet and navbar

--- src/main/java/tech/codeblack/codeblacktech/controllers/IndexController.java
+++ src/main/java/tech/codeblack/codeblacktech/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package tech.codeblack.codeblacktech.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+@CrossOrigin("http://localhost:3000/")
+public class IndexController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "index";
     }
 }
