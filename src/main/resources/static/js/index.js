a3e5684899adea7f66942a94fc442af18d294faf
Rodriques <rodriques.perry@gmail.com>
2022-03-02 20:24:17 -0600

    update:revising homepage

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,10 @@
+let jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    let scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
+
